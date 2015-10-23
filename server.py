ab217f0f536ef89f2aac951bbe5611e7188a494c
Hackbright Student <info@hackbrightacademy.com>
2015-10-22 17:56:57 -0700

    progress on login/user_info pages

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 from jinja2 import StrictUndefined
 
 from flask import Flask, render_template, redirect, request, flash, session
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 
 from model import User, Rating, Movie, connect_to_db, db
 
@@ -38,60 +38,48 @@ def movie_list():
     movies = Movie.query.all()
     return render_template("movie_list.html", movies=movies)
 
-@app.route('/signin')
+# same as the first /register
+@app.route('/signin') 
 def signin():
     """Signin."""
-
+    #flash("testing /signin flash")
     return render_template("signin.html")
+
+
 
 @app.route('/process_signin', methods=["POST"])
 def process_signin():
     """Signin."""
-    username = request.form.get('username')
-    password = request.form.get('password')
-
-    # If user table has username in it already:
-    #   if password is in same row as username (i.e. it's correct)
-    #        say "welcome username"...
-    #   else:
-    #         say "wrong password"
-    #        
-    # else: # if the username isn't even in the table
-    #        say "invalid login information"
-            # try again or new account
-    # insert username and password into the table
+    username = request.form['username']
+    password = request.form['password']
+    print username, password
     
-    return password, username
 
-@app.route("/user-add", methods=['POST'])
-def user_add():
-    """Add a student."""
+    current_user = User.query.filter_by(email=username).first()
+    print current_user
+    print "whatup"
+    
 
-    github = request.form.get('github')
-    first = request.form.get('first')
-    last = request.form.get('last')
-
-    hackbright.make_new_student(first, last, github)
-
-    return render_template("student_added.html",
-                           github=github)
+    print "/users/%d" % current_user.user_id
+    return redirect("/users/%s" % current_user.user_id)
 
 
-def make_new_user(username,password):
-    """Add a new user and print confirmation.
+@app.route("/users/<int:user_id>")
+def show_user(user_id):
+    """Return page showing the details of a given user.
 
-    Given a username and password, add user to the
-    database and print a confirmation message.
+    Show all info about a user. Also, provide a button to logout.
     """
+    print user_id
+    # make logout button here ---------------------------
 
-    QUERY = """INSERT INTO User VALUES (:username, :password)"""
-    db_cursor = db.session.execute(QUERY, {'email': username, 'password': password})
-    db.session.commit()
-    print "Successfully added user: %s" % (username)
+    current_user = User.query.filter_by(user_id=user_id).first()
+    print current_user
+    return render_template("user_info.html", current_user=current_user)
 
 
 
-
+# same as the second /register
 @app.route('/make_account', methods=["POST"])
 def make_account():
 
@@ -99,30 +87,33 @@ def make_account():
     password = request.form.get('password')
     age = request.form.get('age')
     zipcode = request.form.get('zipcode')
-
 # to do: insert this data into database
 
+    new_user = User(email=email, password=password, age=age, zipcode=zipcode)
 
-    db.session.add()
+    db.session.add(new_user)
     db.session.commit()
-    redirect confirm_new_user.html
 
-@app.route('/confirm_new_user')
-def confirm_new_user(username):
+    flash("User %s added." % email)
+    return redirect("/")
+
+
+
+
+@app.route('/logout', methods=["POST"])
+def logout():
+    """Logout."""
     
-
-    print "Hey %s You successfully made a new account. " % (username)
-
-
+    pass
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
-    app.debug = True
+    # app.debug = True
 
     connect_to_db(app)
 
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    # DebugToolbarExtension(app)
 
     app.run()
